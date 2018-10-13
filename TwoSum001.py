9e0181bb9a206fc7ff4f3aef5ffa9a380a0bbf4b
midasevil <midasevil@163.com>
2018-10-13 14:54:17 +0800

    add TwoSum001.py

---
+++ TwoSum001.py
@@ -0,0 +1,39 @@
+#   File Name:           TwoSum     
+#   Description:
+#   Author:              midasevil
+#   date:                2018/10/13
+#
+
+import  pytest
+
+__author__ = 'midasevil'
+
+
+class Solution(object):
+    def twoSum_brute(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        l = len(nums)
+        for i in range(0, l - 1):
+            for j in range(i + 1, l):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+    def twoSum_hash(self, nums, target):
+        """
+        :param nums: List[int]
+        :param target: int
+        :return: List[int
+        """
+        map = dict()
+        l = len(nums)
+        for i in range(l):
+            if nums[i] in map:
+                return [map[nums[i]],i]
+            else:
+                map[target-nums[i]] = i
+
+

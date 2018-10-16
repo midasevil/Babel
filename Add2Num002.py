41a9204ed00f0d170fa8e0edcfadf5d067fd5874
midasevil <midasevil@163.com>
2018-10-16 22:49:58 +0800

    add Add2Num002.py

---
+++ Add2Num002.py
@@ -0,0 +1,36 @@
+#   File Name:           Add2Num002     
+#   Description:
+#   Author:              midasevil
+#   date:                2018/10/14
+#
+
+__author__ = 'midasevil'
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        dummy = cur = ListNode(0)
+        carry = 0
+        while l1 or l2 or carry:
+            val = (l1.val if l1 else 0) + (l2.val if l2 else 0) + carry
+            carry = val // 10
+            cur.next =  ListNode(val % 10)
+            cur = cur.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        return dummy.next
+
+
+
